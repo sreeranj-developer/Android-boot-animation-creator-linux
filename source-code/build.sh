dfc9029b8b011876596638abf3bcd40f8721f198
sreeranj krishna M <91899799+sreeranj-developer@users.noreply.github.com>
2022-02-11 16:04:15 +0530

    Add files via upload

---
+++ source-code/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "building-deb.."
+sleep 5s;
+echo "compressing-data"
+sleep 1s;
+echo "checking-preinst"
+cd build
+cd DEBIAN
+chmod +x preinst
+cd ..
+cd ..
+sleep 1s;
+echo "reading-control"
+sleep 1s;
+dpkg-deb --build build android-bootanimation-creator-V1.1.stable-linux.deb
+sleep 1s;
+echo "creating-md5sum"
+md5sum *.deb > md5sums.txt
+sleep 2s;
+echo "build-finished!"
